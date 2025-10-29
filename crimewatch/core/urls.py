1d6c21a4c699c0a4fec39ccde6ace373cda957ee
Your Name <you@example.com>
2025-10-29 13:45:23 +0300

    update

--- crimewatch/core/urls.py
+++ crimewatch/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('cases/', views.CrimeCaseListView.as_view(), name='case_list'),
     path('about/', views.about_view, name='about'),
     path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
